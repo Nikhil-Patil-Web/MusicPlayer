6613fe93e6bdb4b846c05627b4859cb971dbb2ed
Nikhil <nikhilganeshpatil@gmail.com>
2023-07-14 21:39:28 +0530

    We are done with the custom hook called useUser.

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -4,7 +4,7 @@ import {
   useSessionContext,
   useUser as useSupaUser,
 } from '@supabase/auth-helpers-react'
-import { createContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { Interface } from 'readline'
 
 type UserContextType = {
@@ -40,4 +40,43 @@ export const MyUserContextProvider = (props: Props) => {
       .select('* prices(*, products(*))')
       .in('status', ['trailing', 'active'])
       .single()
+
+  useEffect(() => {
+    if (user && !isLoadingData && !userDetails && !subscription) {
+      setIsLoadingData(true)
+      Promise.allSettled([getUserDetails(), getSubscription()]).then(
+        (results) => {
+          const userDetailsPromise = results[0]
+          const subscriptionPromise = results[1]
+          if (userDetailsPromise.status === 'fulfilled') {
+            setUserDetails(userDetailsPromise.value.data as UserDetails)
+          }
+          if (subscriptionPromise.status === 'fulfilled') {
+            setSubscription(subscriptionPromise.value.data as Subscription)
+          }
+          setIsLoadingData(false)
+        }
+      )
+    } else if (!user && !isLoadingUser && !isLoadingData) {
+      setUserDetails(null)
+      setSubscription(null)
+    }
+  }, [user, isLoadingUser])
+
+  const value = {
+    accessToken,
+    user,
+    userDetails,
+    isLoading: isLoadingUser || isLoadingData,
+    subscription,
+  }
+  return <UserContext.Provider value={value} {...props}></UserContext.Provider>
+}
+
+export const useUser = () => {
+  const context = useContext(UserContext)
+  if (context === undefined) {
+    throw new Error('useUser must be within a myUserContextProvider')
+  }
+  return context
 }
